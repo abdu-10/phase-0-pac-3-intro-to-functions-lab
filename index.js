d49efffaa785e5d699572a60f619bf5aaf97beb4
abdulrahman <abdulrahman.omar@student.moringschool.com>
2022-11-20 15:24:37 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string){
+    if(string.toLowerCase());
+    return"I can\'t hear you!"
+}
+function sayHiToHeadphonedRoommate(string){
+    if(string.UpperCase());
+    return"YES INDEED!"}
+
+ function sayHiToHeadphonedRoommate(string){
+        if(string ="Let\'s have dinner together!")
+        return"I would love to!"}
+
+function sayHiToHeadphonedRoommate(string){
+    const notHearing="I can\'t hear you!"
+    const yes="YES INDEED!"
+    const would= "I would love to!"
+    if(string.toLowerCase(string)===string){
+        return notHearing
+    }
+    else if(string.toUpperCase(string)===string)
+    return yes
+    else if (string="Let\'s have dinner together!"===string){
+        return would
+    }
+}
+
+
+    
